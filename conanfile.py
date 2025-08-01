fc5b7513c028b9426718480b5f7339ea5d3f8716
stevn <smcelholm@gmail.com>
2025-08-01 09:22:34 +0200

    Remove fpic for shared.

--- conanfile.py
+++ conanfile.py
@@ -38,10 +38,6 @@ class TiffWriterConan(ConanFile):
         if self.settings.os == "Windows":
             self.options.rm_safe("fPIC")
 
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-
     def layout(self):
         cmake_layout(self)
 
